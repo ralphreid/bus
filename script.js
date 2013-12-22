d2c53ac6b6d1da363ac9dbc78c59c341236f4fa0
Ralph Reid <beresfordjunior@me.com>
2013-12-22 16:13:40 +0000

    built intitial functions to handle mapoptions, canvas, current location and basic error location handling

--- script.js
+++ script.js
@@ -1,5 +1,51 @@
 $(function(){
 
+  var mapOptions = {
+    zoom: 8,
+    center: new google.maps.LatLng(51.508, -0.120),
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  }
+
+  canvas = document.getElementById("googleMap");
+  map = new google.maps.Map(canvas, mapOptions);
+
+  function updateLocation(position) {
+    var coords = position.coords;
+    var latlng = new google.maps.LatLng(coords.latitude, coords.longitude);
+    var marker = new google.maps.Marker({
+      position: latlng,
+      map: map
+    })
+    map.setCenter(latlng);
+    map.setZoom(17);
+  }
+
+  function handleErrorLocation(error) {
+    console.log(error);
+  }
+
+  $('#current_location').on('click', function() {
+    if(navigator.geolocation){
+      navigator.geolocation.getCurrentPosition(updateLocation, handleErrorLocation);
+      navigator.geolocation.watchPosition(updateLocation, handleErrorLocation);
+    } else {
+      alert("I leave you in the 90's");
+    }
+  })
+
 
 
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
